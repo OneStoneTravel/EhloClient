ad9685ec9f7b5b7c8796a8c9b61a631a27bea3fd
OneStoneTravel <OneStone.TravelCo@gmail.com>
2026-07-27 01:43:49 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ function EhloShell({ session }) {
       <div className="knoxbar">
         <div className="knoxbar-inner">
           <div>
-            <div className="knox-logo">Ehl<span>o</span> <span className="version-tag">v1.2</span></div>
+            <div className="knox-logo">Ehl<span>o</span> <span className="version-tag">v1.3</span></div>
             <div className="knox-sub">OneStone Client Accounts</div>
           </div>
           <div className="knox-user">
